8308eaca0c0ae8e57cac0cf87314056436058a08
farukh <farukh022198@gmail.com>
2023-10-26 18:50:50 -0400

    Building our React App

--- packages/frontend/src/config.ts
+++ packages/frontend/src/config.ts
@@ -1,4 +1,7 @@
 const config = {
+    STRIPE_KEY: "pk_test_51O1WL9C9ZF1XHuJ2ecF6JOpHgWiiuKYY5QlwCiOSk0bkEPRGfbaxyqXKdKn5cglr9oThXkBoijrmDQkL0IlquFIy0010rWqsZR",
+    // Frontend config
+    MAX_ATTACHMENT_SIZE: 5000000,
     // Backend config
     s3: {
       REGION: import.meta.env.VITE_REGION,
@@ -14,6 +17,9 @@ const config = {
       APP_CLIENT_ID: import.meta.env.VITE_USER_POOL_CLIENT_ID,
       IDENTITY_POOL_ID: import.meta.env.VITE_IDENTITY_POOL_ID,
     },
+    
   };
+  
+  
   
   export default config;
