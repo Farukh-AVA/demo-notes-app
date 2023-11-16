f30b357b369952288b0d0f77635dd87e7563c42a
farukh <farukh022198@gmail.com>
2023-11-15 21:37:11 -0500

    implementing dark/light mode

--- packages/frontend/src/App.tsx
+++ packages/frontend/src/App.tsx
@@ -1,25 +1,27 @@
+import 'react-toggle/style.css';
 import Navbar from "react-bootstrap/Navbar";
 import Routes from "./Routes.tsx";
 import Nav from "react-bootstrap/Nav";
+import Toggle from 'react-toggle';
+import { BsBrightnessHigh, BsBrightnessHighFill  } from "react-icons/bs";
 import { LinkContainer } from "react-router-bootstrap";
 import { useState, useEffect } from "react";
 import { AppContext, AppContextType } from "./lib/contextLib";
 import { Auth } from "aws-amplify";
 import { useNavigate } from "react-router-dom";
 import { onError } from "./lib/errorLib";
-
-
-
 import "./App.css";
 
 function App() {
   const [isAuthenticating, setIsAuthenticating] = useState(true);
   const [isAuthenticated, userHasAuthenticated] = useState(false);
+  const [darkMode, setDarkMode] = useState(false);
+  
   useEffect(() => {
     onLoad();
   }, []);
-  const nav = useNavigate();
-  
+  const nav = useNavigate(); 
+ 
   async function onLoad() {
     try {
       await Auth.currentSession();
@@ -32,6 +34,7 @@ function App() {
   
     setIsAuthenticating(false);
   }
+
   async function handleLogout() {
     await Auth.signOut();
   
@@ -39,12 +42,17 @@ function App() {
 
     nav("/login");
   }
+
+  function toggleDarkMode(){
+    setDarkMode(!darkMode);
+  }
+//fw-bold text-muted
   return (
     !isAuthenticating && (
       <div className="App container py-3">
-        <Navbar collapseOnSelect bg="light" expand="md" className="mb-3 px-3">
+        <Navbar collapseOnSelect bg={darkMode ? "dark" : "light"} expand="md" className="mb-3 px-3">
           <LinkContainer to="/">
-            <Navbar.Brand className="fw-bold text-muted">Scratch</Navbar.Brand>
+            <Navbar.Brand className={darkMode ? "WhiteHeader" : "fw-bold text-muted"}>Scratch</Navbar.Brand>
           </LinkContainer>
           <Navbar.Toggle />
           <Navbar.Collapse className="justify-content-end">
@@ -52,22 +60,28 @@ function App() {
               {isAuthenticated ? (
               <>
                 <LinkContainer to="/settings">
-                  <Nav.Link>Settings</Nav.Link>
+                  <Nav.Link className={darkMode ? "NavWhiteFont" : ""}>Settings</Nav.Link>
                 </LinkContainer>
-                <Nav.Link onClick={handleLogout}>Logout</Nav.Link>
+                <Nav.Link onClick={handleLogout} className={darkMode ? "NavWhiteFont" : ""}>Logout</Nav.Link>
               </>
               ) : (
                 <>
                   <LinkContainer to="/signup">
-                    <Nav.Link>Signup</Nav.Link>
+                    <Nav.Link className={darkMode ? "NavWhiteFont" : ""}>Signup</Nav.Link>
                   </LinkContainer>
                   <LinkContainer to="/login">
-                    <Nav.Link>Login</Nav.Link>
+                    <Nav.Link className={darkMode ? "NavWhiteFont" : ""}>Login</Nav.Link>
                   </LinkContainer>
                 </>
               )}
             </Nav>
           </Navbar.Collapse>
+          <Toggle
+                    checked={darkMode}
+                    icons={{checked: <BsBrightnessHigh className="WhiteIcon" />, unchecked: <BsBrightnessHighFill className="BlackIcon" />}}
+                    onChange={toggleDarkMode}
+                    className="custom-toggle"
+                  />
         </Navbar>
         <AppContext.Provider
           value={{ isAuthenticated, userHasAuthenticated } as AppContextType}
